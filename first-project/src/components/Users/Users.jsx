a92757fe3a072d17757e13d3cadd2d1c866df2af
Dmitrii <belovd90@mail.ru>
2021-09-07 02:08:18 +0300

    add: added Users on User Page in BLL

--- first-project/src/components/Users/Users.jsx
+++ first-project/src/components/Users/Users.jsx
@@ -2,28 +2,7 @@ import React from "react";
 
 let Users = (props) => {
     return <div>
-        {
-            props.users.map( u => <div key = {u.id}>
-                <span>
-                    <div>
-                        <img src=""></img>
-                    </div>
-                    <div>
-                        <button>Подписаться</button>
-                    </div>
-                </span>
-                <span>
-                    <span>
-                        <div>{u.fullName}</div>
-                        <div>{u.status}</div>
-                    </span>
-                    <span>
-                        <div></div>
-                        <div></div>
-                    </span>
-                </span>
-            </div>)
-        }
+        Пользователи
     </div>
 }
 
