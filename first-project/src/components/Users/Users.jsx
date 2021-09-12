a337027e772c2595e7703e2c621885fa25bacda5
Dmitrii <belovd90@mail.ru>
2021-09-13 02:51:34 +0300

    added axios. API, request to server

--- first-project/src/components/Users/Users.jsx
+++ first-project/src/components/Users/Users.jsx
@@ -1,14 +1,23 @@
+import *as axios from "axios";
 import React from "react";
 import styles from "./users.module.css";
+import userPhoto from "../../assets/images/users.png";
 
 let Users = (props) => {
+    
+    if(props.users.length === 0) {
+        axios.get('https://social-network.samuraijs.com/api/1.0/users').then(response => {
+            props.setUsers(response.data.items);
+        });
+    }
+
 
     return <div>
         {
             props.users.map( u => <div key={u.id}>
                 <span>
                     <div>
-                        <img src={u.photoUrl} className={styles.userPhoto} alt="avatar" />
+                        <img src={u.photos.small !=null ? u.photos.small : userPhoto } className={styles.userPhoto} alt="avatar" />
                     </div>
                     <div>
                         { u.followed 
@@ -18,12 +27,12 @@ let Users = (props) => {
                 </span>
                 <span>
                     <span>
-                        <div>{u.fullName}</div>
+                        <div>{u.name}</div>
                         <div>{u.status}</div>
                     </span>
                     <span>
-                        <div>{u.location.country}</div>
-                        <div>{u.location.city}</div>
+                        <div>{"u.location.country"}</div>
+                        <div>{"u.location.city"}</div>
                     </span>
                 </span>
             </div>)
--- first-project/src/redux/users-reducer.js
+++ first-project/src/redux/users-reducer.js
@@ -2,16 +2,18 @@ const FOLLOW = "FOLLOW";
 const UNFOLLOW = "UNFOLLOW";
 const SET_USERS = "SET_USERS";
 
-let initialState = { users: [
-    {id: 1, photoUrl:'https://cdn1.mecum.com/auctions/pa0715/pa0715-216947/images/pa0715-216947_9@2x.jpg?1438443018000', 
-        followed:true, fullName: "Дмитрий", status: "Один из немногих", location: {city:"Saint-Petersburg", country: "Russia"} },
-    {id: 2, photoUrl:'https://catherineasquithgallery.com/uploads/posts/2021-02/1612265066_104-p-mashina-na-fioletovom-fone-149.jpg', 
-        followed:false, fullName: "Тусяныч", status: "Настроение Среднее", location: {city:"Magnitogorsk", country: "Russia"} },
-    {id: 3, photoUrl:'https://c.wallhere.com/photos/10/6e/car_Dodge_Challenger_red_cars_Dodge-248478.jpg!d', 
-        followed:true, fullName: "Филар", status: "Вечно веселый", location: {city:"Pushkin", country: "Russia"} },
-    {id: 4, photoUrl:'https://a.d-cd.net/e406914s-960.jpg', 
-        followed:false, fullName: "Абдул", status: "Хочу найти друзей", location: {city:"Kemer", country: "Turkey"} },
-]};
+let initialState = { 
+    users: [
+//     {id: 1, photoUrl:'https://cdn1.mecum.com/auctions/pa0715/pa0715-216947/images/pa0715-216947_9@2x.jpg?1438443018000', 
+//         followed:true, fullName: "Дмитрий", status: "Один из немногих", location: {city:"Saint-Petersburg", country: "Russia"} },
+//     {id: 2, photoUrl:'https://catherineasquithgallery.com/uploads/posts/2021-02/1612265066_104-p-mashina-na-fioletovom-fone-149.jpg', 
+//         followed:false, fullName: "Тусяныч", status: "Настроение Среднее", location: {city:"Magnitogorsk", country: "Russia"} },
+//     {id: 3, photoUrl:'https://c.wallhere.com/photos/10/6e/car_Dodge_Challenger_red_cars_Dodge-248478.jpg!d', 
+//         followed:true, fullName: "Филар", status: "Вечно веселый", location: {city:"Pushkin", country: "Russia"} },
+//     {id: 4, photoUrl:'https://a.d-cd.net/e406914s-960.jpg', 
+//         followed:false, fullName: "Абдул", status: "Хочу найти друзей", location: {city:"Kemer", country: "Turkey"} },
+    ]
+};
 
 const usersReducer = (state = initialState, action) => {
     switch (action.type) {
@@ -35,11 +37,9 @@ const usersReducer = (state = initialState, action) => {
                     return u;
                 })
             }
-        case SET_USERS: 
-            return {
-                ...state, 
-                users: [...state.users, ...action.users]
-            }
+        case SET_USERS: {
+            return {...state, users: [...state.users , ...action.users]}
+         }
 
         default:
             return state;
